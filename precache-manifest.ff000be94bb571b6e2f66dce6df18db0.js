cae5eddf142f953a76a13ca974fc26ad0047c498
13circle <13circle97@gmail.com>
2020-09-25 01:35:59 +0900

    Updates

---
+++ precache-manifest.ff000be94bb571b6e2f66dce6df18db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c377e3905de6009bf2d94122ac7fc4",
+    "url": "/velopert-todo-list-react/index.html"
+  },
+  {
+    "revision": "af57138d31827fc2f493",
+    "url": "/velopert-todo-list-react/static/css/main.1de0f9ad.chunk.css"
+  },
+  {
+    "revision": "3f3758614cc85c580652",
+    "url": "/velopert-todo-list-react/static/js/2.8f2ac878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/velopert-todo-list-react/static/js/2.8f2ac878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af57138d31827fc2f493",
+    "url": "/velopert-todo-list-react/static/js/main.1afe9ea5.chunk.js"
+  },
+  {
+    "revision": "95fea592cb42b7163afe",
+    "url": "/velopert-todo-list-react/static/js/runtime-main.d8e9eaa5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/velopert-todo-list-react/precache-manifest.ff000be94bb571b6e2f66dce6df18db0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/velopert-todo-list-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
